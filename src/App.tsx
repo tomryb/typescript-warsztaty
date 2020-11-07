5b73453b20a5c7391a43a4baa25f9dc9ac07258d
tomryb <1rybak.tomasz@gmail.com>
2020-11-07 10:19:31 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import React from 'react';
 function App() {
   // const add = (a: number, b: number) => a + b //1 przykład
   // const add = (a: number, b?:number) => b ? a+b : a //zadanie 1
-  const add = (...rest: number[]) => {
 
-    let result: number = 0;
-    rest.map((element: number) => {
-      result += element
-    })
-    return result
-  } //zadanie 1 extra //
+  // zadanie 1 extra
+  // const add = (...rest: number[]) => {
+
+  //
+  //   let result: number = 0;
+  //   rest.map((element: number) => {
+  //     result += element
+  //   })
+  //   return result
+  // }
 
 
   return (
     <div className="App">
-      {/* {add(4, 5)}  */}
-      {add(5, 5, 5)}
+      {/* {add(5, 5, 5)} */}
     </div>
   );
 }
