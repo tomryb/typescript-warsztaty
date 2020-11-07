a852b25fa3ba2063d7fd4721a8427f32c4001b6a
tomryb <1rybak.tomasz@gmail.com>
2020-11-07 09:50:16 +0100

    1 przykład

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function App() {
+  const add = (a: number, b: number) => a + b //1 przykład
+
   return (
     <div className="App">
-
+      {add(4, 5)}
     </div>
   );
 }
